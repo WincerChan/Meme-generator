7bda25817b5ddb34be039e4fc8392040ea8d5bb8
WincerChan <wincerchan@gmail.com>
2018-05-20 16:52:51 +0800

    Change ui interface

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: {
         app: "./app.js"
@@ -9,4 +11,13 @@ module.exports = {
         path: path.resolve(__dirname, "public/dist"),
         filename: "bundle.js"
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract('css-loader')
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+    ]
 };
