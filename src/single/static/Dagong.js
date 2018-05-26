8fcccc83504955105b442eb2e14474befb07e67d
WincerChan <wincerchan@gmail.com>
2018-05-26 11:48:29 +0800

    Add some templates

---
+++ src/single/static/Dagong.js
@@ -0,0 +1,40 @@
+var Dagong = {
+    'name': '打工是不可能打工的',
+    'component': undefined,
+    'url': '/38655051',
+    'gif': '/template/dg.gif',
+    'config': [
+        {
+            "default": "没有钱啊，肯定要做的啊",
+            "startTime": 0,
+            "endTime": 1.8
+        },
+        {
+            "default": "不做的话没有钱用",
+            "startTime": 1.88,
+            "endTime": 3.76
+        },
+        {
+            "default": "那你不会去打工啊",
+            "startTime": 3.81,
+            "endTime": 4.66
+        },
+        {
+            "default": "有手有脚的",
+            "startTime": 4.66,
+            "endTime": 5.90
+        },
+        {
+            "default": "打工是不可能打工的",
+            "startTime": 6.02,
+            "endTime": 8.42
+        },
+        {
+            "default": "这辈子都不可能打工的",
+            "startTime": 8.42,
+            "endTime": 10.75
+        }
+    ]
+}
+
+export { Dagong }
---
+++ src/single/static/Lianliankan.js
@@ -0,0 +1,30 @@
+var Lianliankan = {
+    'name': '星际还是魔兽',
+    'component': undefined,
+    'url': '/50ccf9b9',
+    'gif': '/template/llk.gif',
+    'config': [
+        {
+            "default": "你平时打电子游戏吗",
+            "startTime": 0.38,
+            "endTime": 1.86
+        },
+        {
+            "default": "偶尔",
+            "startTime": 2.93,
+            "endTime": 3.70
+        },
+        {
+            "default": "星际还是魔兽",
+            "startTime": 3.73,
+            "endTime": 5.63
+        },
+        {
+            "default": "连连看",
+            "startTime": 6.97,
+            "endTime": 7.79
+        }
+    ]
+}
+
+export { Lianliankan }
